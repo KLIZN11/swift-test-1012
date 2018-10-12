7dfcff1cbf51c154cd1d3770f6c1e8057539e6f3
KLIZN11 <s20171103188@king-2.local>
2018-10-12 19:34:38 +0800

    Initial Commit

---
+++ swift-test-1012/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-test-1012
+//
+//  Created by s20171103188 on 2018/10/12.
+//  Copyright © 2018年 s20171103188. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
